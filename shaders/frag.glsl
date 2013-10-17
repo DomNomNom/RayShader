636c9e6dac6484969a2f485c7c45146ca160978d
Dominik Schmid <DominikSchmid93@gmail.com>
2013-10-18 04:02:30 +1300

    fixed AABB

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -149,7 +149,7 @@ ret trace_water(vec4 eye, vec4 dir) {
     ret r = noHit();
 
     // AABB optimization
-    if ( hitAABB(
+    if (! hitAABB(
         eye,
         dir,
         vec3(water[0         ].x, turbulent_min, -1.0),
@@ -187,11 +187,11 @@ ret trace_water(vec4 eye, vec4 dir) {
                     water_normals[i+1],
                     smoothstep(0.0, 1.0, u)
                 )
-                // + 0.1*vec3( // add a fake ripples
-                //     cos((intersect.x+intersect.z*0.3)*13.0 + time*-3.0),
-                //     0.0,
-                //     cos((intersect.x)*7.0 + time*4.0) + sin(intersect.z*10.0 +time*1.0)
-                // )
+                + 0.1*vec3( // add a fake ripples
+                    cos((intersect.x+intersect.z*0.3)*13.0 + time*-3.0),
+                    0.0,
+                    cos((intersect.x)*7.0 + time*4.0) + sin(intersect.z*10.0 + time*1.0)
+                )
             ), 0.0);
         }
     }
